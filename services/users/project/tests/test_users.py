d5447e9d2cb7515583641efcb7e3d7b6e84fe1c3
Celoka <eloka.chima@gmail.com>
2018-06-10 02:21:28 +0100

    feature(route): add home route
    - modify users .py file to include a new route handler for home or index
    - create an html file for it

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -137,5 +137,30 @@ class TestUserService(BaseTestCase):
             self.assertIn('chris@yahoo.com', data['data']['users'][2]['email'])
             self.assertIn('success', data['status'])
 
+    def test_main_no_users(self):
+        """Ensure the home route behaves as expected """
+        add_user('eloka', 'eloka@yahoo.com')
+        add_user('ebuka', 'ebuka@yahoo.com')
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'eloka', response.data)
+            self.assertIn(b'ebuka', response.data)
+
+    def test_main_add_user(self):
+        """Ensure a new user ca be added to the database"""
+        with self.client:
+            response = self.client.post(
+                '/',
+                data = dict(username='chris', email='chris@yahoo.com'),
+                follow_redirects=True
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'chris', response.data)
+
 if __name__ == '__main__':
     unittest.main()
