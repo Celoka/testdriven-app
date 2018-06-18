f4b6dabdc7e6eefb3386d6ea9f622c6fa9d3aa34
Celoka <eloka.chima@gmail.com>
2018-06-18 16:56:35 +0100

    feature(react-setup): setup react
    - setup react

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 
 # instantiate the db
@@ -13,6 +14,8 @@ def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+
+    CORS(app)
 
     # set config
     app_settings = os.getenv('APP_SETTINGS')
